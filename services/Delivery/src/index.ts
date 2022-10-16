3651150ed412bd981d1106f909fab5c7bfa4e729
Hannes Eckelt <fhs49066@fh-salzburg.ac.at>
2022-10-16 15:29:24 +0200

    feature(index.js) => logic to add multiple orders for one guest is basically done !BUG is included

--- services/Delivery/src/index.ts
+++ services/Delivery/src/index.ts
@@ -35,40 +35,105 @@ app.get<any, OutgoingRequestResponse, OutgoingRequestRequestBody>("/outgoingRequ
 });
 //////////////////////////////////////Template Code/////////////////////////////////////////////////////////////
 
+//////////////////////////////////////deliveryInformation endpoint//////////////////////////////////////////////
+interface GuestDelivery {
+    guest: number,
+    deliveries: Delivery[]
+}
+interface Delivery {
+    delivery: number,
+    food: number[],
+    drink: number[]
+}
 interface DeliveryAndDrinksInformation {
     guest: number;
     food: number[];
-    drinks: number[];
+    drink: number[];
 }
+
+
+const guestDeliveries: GuestDelivery[] = [];
+
 let information: DeliveryAndDrinksInformation;
 app.post("/deliveryInformation", (req, res) => {
-    information = req.body;
+    const receivedInformation = req.body;
+    const guestDelivery: GuestDelivery = createNewGuestWithDelivery(receivedInformation);
 
-    //Creating the url based on post input
+    //TODO: Change this URL, when deploying it
     const originUrl = "http://localhost:5000";
     const urlParams = {
-        guest: information.guest,
-        order: '1'
+        guest: guestDelivery.guest,
+        delivery: guestDelivery.deliveries[0].delivery
     }
-    const url = new URL(`${originUrl}/guest/${urlParams.guest}/deliveries/${urlParams.order}`)
 
-     fetch(url.href, {
+    const drinksDelivery = {
+        guest: guestDelivery.guest,
+        food: [] as number[],
+        drinks: guestDelivery.deliveries[0].drink
+    }
+    const url = new URL(`${originUrl}/guest/${urlParams.guest}/deliveries/${urlParams.delivery}`)
+
+    fetch(url.href, {
         method: 'POST',
-        body: JSON.stringify(information),
-        headers: {'Content-Type': 'application/json'}
+        body: JSON.stringify(drinksDelivery),
+        headers: { 'Content-Type': 'application/json' }
     })
-
     res.send({ message: "Information was send successfully!" })
 })
 
-interface PreparedFood{
+//TODO: This double find method on array can be simpliefied, by handing an method into the find method
+//See the prime number example in the find() Documentation https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find?retiredLocale=de
+
+function addDelivery(deliveryInformation: DeliveryAndDrinksInformation) {
+    let addedDelivery: GuestDelivery;
+    if (guestDeliveries.find(order => order.guest === deliveryInformation.guest)) {
+        addedDelivery = addDeliveryToGuest(deliveryInformation)
+    }
+    else {
+        addedDelivery = createNewGuestWithDelivery(deliveryInformation);
+    }
+}
+
+function createNewGuestWithDelivery(deliveryInformation: DeliveryAndDrinksInformation) {
+    const guestDelivery: GuestDelivery = {
+        guest: deliveryInformation.guest,
+        deliveries: [{
+            delivery: 1,
+            food: deliveryInformation.food,
+            drink: deliveryInformation.drink
+        }]
+    }
+    guestDeliveries.push(guestDelivery);
+    return guestDelivery;
+}
+
+//BUG:This does not work right now, because the whole array is expected and not a single Item
+function addDeliveryToGuest(deliveryInformation: DeliveryAndDrinksInformation) {
+    let deliveryItem;
+    guestDeliveries.forEach((element) => {
+        if (element.guest === deliveryInformation.guest) {
+            deliveryItem = {
+                delivery: +element.deliveries.length,
+                food: deliveryInformation.food,
+                drink: deliveryInformation.drink
+            }
+            element.deliveries.push(deliveryItem);
+        }
+    }
+    )
+    return { guest: deliveryInformation.guest, deliverydeliveryItem };
+}
+//////////////////////////////////////deliveryInformation endpoint//////////////////////////////////////////////
+
+//////////////////////////////////////preparedNotification endpoint/////////////////////////////////////////////
+interface PreparedFood {
     guest: number,
     food: number
 }
-app.post("/preparedNotification", (req,res)=>{
-const preparedFood: PreparedFood= req.body;
-res.send({ message: "Notification was send successfully!" })
-console.log(preparedFood);
+app.post("/preparedNotification", (req, res) => {
+    const preparedFood: PreparedFood = req.body;
+    res.send({ message: "Notification was send successfully!" })
+    console.log(preparedFood);
 })
 
 
@@ -76,4 +141,5 @@ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
-//Todo: Create callable endpoint which reseives deliveryInformation
+
+//////////////////////////////////////preparedNotification endpoint/////////////////////////////////////////////
