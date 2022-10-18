0f8caf770086d7b59691ac11758dc43007819bb7
fhs49302 <fhs49302@fh-salzburg.ac.at>
2022-10-18 16:06:48 +0200

    Added api implemtation

---
+++ services/TableService/src/order.ts
@@ -0,0 +1,46 @@
+export async function order(req: { json: () => any; }){
+    const order = req.json();
+    const highestOrder = await getFood(order);
+    return calculateWaitingTime(highestOrder);
+
+}
+
+async function sendOrderToDelivery(order: { guest?: number; food: any; drink?: number[]; }){
+    fetch("Delivery:8084/orderInformation", {
+        method: "POST",
+        body: JSON.stringify(order),
+        headers: {"Content-Type" : "application/json"}
+    });
+    // create order number for each order
+}
+
+async function getFood(order: { guest?: number; food: any; drink?: number[]; }){
+    //send food order to food preparation
+    const foodOrder = await order.food;
+    let highestOrder = 0;
+
+    foodOrder.forEach((element: any) => {
+        fetch("FoodPreparation:8085/orderItem", {
+            method: "POST",
+            body: JSON.stringify({id: element}),
+            headers: {"Content-Type" : "application/json"}
+        })
+        .then(async (response) => {
+            const orderPosition = await response.json();
+            if(orderPosition > highestOrder){
+                highestOrder = orderPosition;
+            }
+        });
+    });
+    return highestOrder;
+}
+
+function calculateWaitingTime(highestOrder: number){
+    let waitingTime = highestOrder * 4;
+    return waitingTime;
+}
+
+
+
+
+
