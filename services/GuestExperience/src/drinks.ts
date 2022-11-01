3e3935365c153a0c117ca7d66e1f51898b57e7b0
Tabea Schaeffer <tabea@briese-schaeffer.de>
2022-11-01 14:35:02 +0100

    Refactoring

--- services/GuestExperience/src/drinks.ts
+++ services/GuestExperience/src/drinks.ts
@@ -1,8 +1,10 @@
+import { NUTRITION } from "./types/types";
+
 export const drinks = [
     {
         id: 1,
         name: "Soda",
-        nutrition: ["H"],
+        nutrition: [NUTRITION.H],
         price: 4.4,
     },
     {
@@ -11,4 +13,10 @@ export const drinks = [
         nutrition: [],
         price: 5.4,
     },
+    {
+        id: 3,
+        name: "Coca Cola",
+        nutrition: [NUTRITION.H, NUTRITION.C],
+        price: 5.4,
+    }
 ];
