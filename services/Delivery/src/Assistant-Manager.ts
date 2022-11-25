4079f1dc7ac1f629d6e80a02312f46f95fca2968
Hannes Eckelt <fhs49066@fh-salzburg.ac.at>
2022-11-25 17:03:42 +0100

    feature(Delivery/Assistant-Manager) => Add fibonacci sequence for delay and added deliveryId in post headrer to billing

--- services/Delivery/src/Assistant-Manager.ts
+++ services/Delivery/src/Assistant-Manager.ts
@@ -29,11 +29,11 @@ export default class AssistantManager {
             console.log("Error: An issue occured by sending delivery to customer!");
         })
 
-        await this.registerDeliveryForBilling(delivery);
+        await this.registerDeliveryForBilling(delivery, deliveryBody.delivery);
         this.isDelivering = false;
     }
 
-    async registerDeliveryForBilling(delivery: GuestWithOrder) {
+    async registerDeliveryForBilling(delivery: GuestWithOrder, deliveryId: number) {
         const originUrl = process.env.API_BILLING || "Billing:8083";
         const urlParams = {
             guest: delivery.guest,
@@ -46,23 +46,20 @@ export default class AssistantManager {
             drinks: delivery.Order.drinks,
             order: delivery.Order.order
         }
-        await this.fetchBilling(url, deliveryBody)
+        await this.fetchBilling(url, deliveryBody, deliveryId)
     }
 
-    async fetchBilling(url: URL, deliveryBody: any) {
-        const retryTimes = [1000, 3000, 9000, 18000, 54000];  //Delay times increase by factor 3 for each iteration
+    async fetchBilling(url: URL, deliveryBody: any, deliveryId: number) {
+        const retryTimes = this.getFibonacciSequence(10).map(number => number * 1000);  //Fibonacci based daly time
         let i = 0;
         let keepInterating = true;
         while (keepInterating) {
-            await fetch("http://httpstat.us/503", {
+            await fetch(url.href, {
                 method: 'POST',
                 body: JSON.stringify(deliveryBody),
-                headers: { 'Content-Type': 'application/json' }
+                headers: { 'Content-Type': 'application/json', 'deliveryId': deliveryId.toString() }
             }).then(async (response) => {
-
                 if (response.status >= 500 && response.status <= 599) {  //Check for error code in the 500 range
-                    console.log("i value is: ", i);
-                    console.log("Delay time is ", retryTimes[i])
                     await this.delayForRetry(retryTimes[i])
                     i++
                 }
@@ -74,16 +71,31 @@ export default class AssistantManager {
                 console.log("Error: An issue occured by sending delivery to billing!");
                 keepInterating = false  //Avoid infinite loop in case of catch
             })
-            if (i > 4) {  //End loop interation if all 5 retry attemps fails
+            if (i >= 10) {  //End loop interation if all 5 retry attemps fails
                 keepInterating = false;
                 console.log("Retry for Billing failed! Message was not send successfully");
             }
+        }
+    }
+    getFibonacciSequence(sequLength: number) {
+        var i;
+        var fib = [0, 1]
+        if (sequLength < 3) {
+            return fib.slice(0, sequLength)
+        }
+        else {
+            for (i = 2; i <= (sequLength - 1); i++) {
+                fib.push(fib[i - 2] + fib[i - 1]);
+            }
+            return fib;
         }
     }
 
     delayForRetry(delay: number) {
         return new Promise((resolve) => { setTimeout(resolve, delay); })
     }
+
+
 }
 //TODO: Add variable is reserved also to the messaging for billing
 // fetching is now outsorced and will be done for three times, but never longer than one minute
